90455bb2f0c8791117073b0714970e42f31f63ff
Alex Eng <aeng@redhat.com>
2014-09-29 13:10:45 +1000

    Add endpoint for version locale list

--- zanata-common-api/src/main/java/org/zanata/rest/service/VersionLocalesResource.java
+++ zanata-common-api/src/main/java/org/zanata/rest/service/VersionLocalesResource.java
@@ -17,16 +17,21 @@ import org.zanata.rest.dto.Locale;
  */
 @Produces({ MediaType.APPLICATION_JSON })
 @Consumes({ MediaType.APPLICATION_JSON })
-public interface LocalesResource {
+public interface VersionLocalesResource {
+    public static final String ITERATION_SLUG_TEMPLATE = "{versionSlug:"
+            + RestConstants.SLUG_PATTERN + "}";
 
-    public static final String SERVICE_PATH = "/locales";
+    public static final String SERVICE_PATH = ProjectResource.SERVICE_PATH
+            + "/iterations/i/" + ITERATION_SLUG_TEMPLATE + "/locales";
 
     /**
-     * Retrieves a full list of locales enabled in Zanata. The result is
+     * Retrieves a full list of locales enabled in version. The result is
      *
      * @return The following response status codes will be returned from this
      *         operation:<br>
      *         OK(200) - Response containing a full list of locales. <br>
+     *         NOT FOUND(404) - If a Version could not be found for the given
+     *         parameters.<br>
      *         INTERNAL SERVER ERROR(500) - If there is an unexpected error in
      *         the server while performing this operation.
      */
