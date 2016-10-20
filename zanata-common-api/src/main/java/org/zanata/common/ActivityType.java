f56cad1a793cee300e845bd2ad6a8b5f62406abb
Sean Flanigan <sflaniga@redhat.com>
2016-10-20 17:37:48 +1000

    Add README pointing to zanata-platform; remove files

--- zanata-common-api/src/main/java/org/zanata/common/ActivityType.java
+++
@@ -1,9 +0,0 @@
-package org.zanata.common;
-
-import javax.xml.bind.annotation.XmlType;
-
-@XmlType(name = "activityType")
-public enum ActivityType {
-    UPDATE_TRANSLATION, REVIEWED_TRANSLATION, UPLOAD_SOURCE_DOCUMENT,
-    UPLOAD_TRANSLATION_DOCUMENT;
-}
--- zanata-common-api/src/main/java/org/zanata/rest/dto/ExtensionValue.java
+++
@@ -1,7 +0,0 @@
-package org.zanata.rest.dto;
-
-import java.io.Serializable;
-
-public interface ExtensionValue extends Serializable {
-
-}
